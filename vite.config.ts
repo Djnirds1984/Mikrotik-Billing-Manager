8d2f1cc8f19d6c849dafad2952170a52a6cbaf7c
Djnirds1984 <aldrincabanez9@gmail.com>
2025-12-08 13:10:17 +0800

    Update allowedHosts in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode, command }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      allowedHosts: ['egestive-lemming-9933.dataplicity.io', 'localhost', '127.0.0.1'],
+      allowedHosts: ['verecund-millipede-5831.dataplicity.io', 'localhost', '127.0.0.1'],
       proxy: {
         '/mt-api': {
           target: 'http://localhost:3001',
